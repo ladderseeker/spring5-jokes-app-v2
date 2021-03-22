3691e33c053e9ffeb7b696707e321f309ea886fe
XIAO <jinxiao.shawn@gmail.com>
2021-03-22 16:38:26 +0800

    Add joke service and implement one.(#3)

--- src/main/java/guru/springframework/spring5jokesappv2/Spring5JokesAppV2Application.java
+++ src/main/java/guru/springframework/spring5jokesappv2/Spring5JokesAppV2Application.java
@@ -1,13 +1,17 @@
 package guru.springframework.spring5jokesappv2;
 
+import guru.springframework.spring5jokesappv2.services.JokeService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Spring5JokesAppV2Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Spring5JokesAppV2Application.class, args);
+        ApplicationContext ctx = SpringApplication.run(Spring5JokesAppV2Application.class, args);
+        JokeService jokeService = (JokeService) ctx.getBean("jokeServiceImpl");
+        System.out.println(jokeService.getJoke());
     }
 
 }
