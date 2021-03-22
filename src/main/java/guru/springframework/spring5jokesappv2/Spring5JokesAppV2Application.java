e372c14e4938f5cb0963e43ea38eb8e2fadb6fe6
XIAO <jinxiao.shawn@gmail.com>
2021-03-22 16:49:21 +0800

    Add joke controller.(#4)

--- src/main/java/guru/springframework/spring5jokesappv2/Spring5JokesAppV2Application.java
+++ src/main/java/guru/springframework/spring5jokesappv2/Spring5JokesAppV2Application.java
@@ -1,17 +1,13 @@
 package guru.springframework.spring5jokesappv2;
 
-import guru.springframework.spring5jokesappv2.services.JokeService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Spring5JokesAppV2Application {
 
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Spring5JokesAppV2Application.class, args);
-        JokeService jokeService = (JokeService) ctx.getBean("jokeServiceImpl");
-        System.out.println(jokeService.getJoke());
+        SpringApplication.run(Spring5JokesAppV2Application.class, args);
     }
 
 }
